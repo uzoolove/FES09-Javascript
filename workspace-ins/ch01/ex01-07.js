16d80ee86fcb8c2774b40693794ae67742d31ba3
Kilyong Jeong <uzoolove@gmail.com>
2024-03-05 09:03:05 +0900

    📝

--- workspace-ins/ch01/ex01-07.js
+++ workspace-ins/ch01/ex01-07.js
@@ -4,16 +4,16 @@ var foo = {
 	major: 'computer science'
 };
 // 객체 프로퍼티 읽기
-console.log(foo.name.toUpperCase(), foo['major'], foo.sub/*.toUpperCase()*/);
+console.log(foo.name.toUpperCase(), foo['major'], foo.sub?.toUpperCase());
 // 객체 프로퍼티 갱신
 foo.major = 'electronic engineering';
-console.log(foo.name.toUpperCase(), foo['major'], foo.sub/*.toUpperCase()*/);
+console.log(foo.name.toUpperCase(), foo['major'], foo.sub?.toUpperCase());
 // 객체 프로퍼티 동적 생성
 foo.sub = 'computer engineering';
-console.log(foo.name.toUpperCase(), foo['major'], foo.sub/*.toUpperCase()*/);
+console.log(foo.name.toUpperCase(), foo['major'], foo.sub?.toUpperCase());
 // 객체 프로퍼티 삭제
 delete foo.major;
-console.log(foo.name.toUpperCase(), foo['major'], foo.sub/*.toUpperCase()*/);
+console.log(foo.name.toUpperCase(), foo['major'], foo.sub?.toUpperCase());
 // 객체 모든 프로퍼티 읽기
 for(var prop in foo){
   console.log(prop, foo[prop]);
