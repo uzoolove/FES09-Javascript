71d29b6edcb563fc5f31c704290aaf5d5d43674d
Kilyong Jeong <uzoolove@gmail.com>
2024-03-12 16:00:44 +0900

    ♻️

--- workspace-ins/ch02/ex02-20-01.js
+++ workspace-ins/ch02/ex02-20-01.js
@@ -22,7 +22,7 @@ var todolist = [
 ]
 
 // sort() _id로 내림차순 정렬
-todolist.sort(function(a, b){
+todolist.sort((a, b) => {
   if(a._id < b._id){
     return 1; // 0보다 큰 수를 반환하면 b를 앞으로
   }else{
@@ -33,7 +33,7 @@ console.log('정렬 결과', todolist);
 
 // forEach() 완료된 할일 목록
 var doneList = [];
-todolist.forEach(function(todo){
+todolist.forEach(todo => {
   if(todo.done){
     doneList.push(todo);
   }
@@ -41,14 +41,14 @@ todolist.forEach(function(todo){
 console.log('forEach() 완료된 할일 목록', doneList);
 
 // filter() 완료된 할일 목록
-var doneList = todolist.filter(function(todo){
+var doneList = todolist.filter((todo) => {
   return todo.done;
 });
 var doneList = todolist.filter(todo => todo.done);
 console.log('filter() 완료된 할일 목록', doneList);
 
 // map() 남은 할일 목록
-var reaminList = todolist.map(function(todo){
+var reaminList = todolist.map((todo) => {
   if(!todo.done){
     return todo;
   }
@@ -58,31 +58,21 @@ var reaminList = todolist.map(function(todo){
 console.log('map() 남은 할일 목록', reaminList);
 
 // reduce() 남은 할일 수
-var reaminCount = todolist.reduce(function(acc, todo){
-  return acc + (todo.done?0:1);
-}, 0);
+var reaminCount = todolist.reduce((acc, todo) => acc + (todo.done ? 0 : 1), 0);
 console.log('reduce() 남은 할일 수', reaminCount);
 
 // find() _id=2인 할일
-var todo = todolist.find(function(todo){
-  return todo._id===2;
-});
+var todo = todolist.find(todo => todo._id===2 );
 console.log('find() _id=2인 할일', todo);
 
 // find() _id=3인 할일의 index
-var todoIndex = todolist.indexOf(todolist.find(function(todo){
-  return todo._id===3;
-}));
+var todoIndex = todolist.findIndex(todo => todo._id===3);
 console.log('find() _id=3인 할일의 index', todoIndex);
 
 // some() 남은 할일이 하나라도 있는가?
-var hasTodo = todolist.some(function(todo){
-  return !todo.done;
-});
+var hasTodo = todolist.some(todo => !todo.done);
 console.log('some() 남은 할일이 하나라도 있는가?', hasTodo);
 
 // every() 할일이 모두 완료 되었는가?
-var busy = todolist.every(function(todo){
-  return todo.done;
-});
+var busy = todolist.every(todo => todo.done);
 console.log('every() 할일이 모두 완료 되었는가?', busy);
